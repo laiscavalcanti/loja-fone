20021002caa4612d0516421db3e4ba248abe92a7
Laís Cavalcanti <cavalcantislais@gmail.com>
2019-01-30 16:33:37 -0300

    [013] finalizada

---
+++ js/description-product.js
@@ -0,0 +1,13 @@
+/*
+ter a janela do - browser -na mão
+Localizar o html;
+Pegar o coração;
+Pegar o click no coração;
+no momento que o usuario clicar, mostrar um texto na tela
+
+*/
+alert('Antes do nosso comando');
+
+const $heart = window.document.querySelector(".-heart");
+
+console.log($heart);
