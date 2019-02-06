dc441fb013dcf8a1711a9588dc6fa6e0343d388a
Laís Cavalcanti <cavalcantislais@gmail.com>
2019-02-06 16:46:02 -0300

    [015] finalizada

--- js/description-product.js
+++ js/description-product.js
@@ -1,15 +1,13 @@
-/*
-ter a janela do - browser -na mão
-Localizar o html;
-Pegar o coração;
-Pegar o click no coração;
-no momento que o usuario clicar, mostrar um texto na tela
 
-*/
 
-const $heart = window.document.querySelector(".-heart");
-
+const $heart = document.querySelector(".-heart");
 $heart.addEventListener("click", handleClick);
+
+    
+function handleClick() {
+   
+    $heart.classList.toggle("-active");
+  }
 
 
 
--- js/home.js
+++ js/home.js
@@ -1,9 +1,11 @@
-
+let $valorInicial = 10;
 const $second = document.querySelector(".-second");
 
 $second.addEventListener("click", handleClick);
 
     function handleClick (){
         const $carrinho = document.querySelector(".-last");
-            $carrinho.textContent = "mudando de valor";
+            $valorInicial++;
+
+            $carrinho.textContent = `Carrinho (${++valorInicial})`;
     }
