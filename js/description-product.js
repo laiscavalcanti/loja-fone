46237a5e48e31f047c757204304f12580208d436
Laís Cavalcanti <cavalcantislais@gmail.com>
2019-02-19 22:23:22 -0300

    [017] finalizada

--- js/description-product.js
+++ js/description-product.js
@@ -1,14 +1,25 @@
-
-
 const $heart = document.querySelector(".-heart");
+const $stars = document.querySelectorAll(".star");
+
 $heart.addEventListener("click", handleClick);
 
-    
 function handleClick() {
-   
-    $heart.classList.toggle("-active");
-  }
+  this.classList.toggle("-active");
+}
 
+$stars.forEach(($star, itemSelected) => {
+  $star.addEventListener("click", starSelected);
+
+  function starSelected() {
+    $stars.forEach((star, index) => {
+      if (itemSelected >= index) {
+        star.classList.add("-active");
+      } else {
+        star.classList.remove("-active");
+      }s
+    });
+  }
+});
 
 
 
