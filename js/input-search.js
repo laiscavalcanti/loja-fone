583910da23443922385819a7f0a4b8f88f55de27
Laís Cavalcanti <cavalcantislais@gmail.com>
2019-02-25 16:31:27 -0300

    atualizado

--- js/input-search.js
+++ js/input-search.js
@@ -1,9 +1,22 @@
 const $inputSearch = document.querySelector(".input-search");
-const $icon = $inputSearch.querySelector(".icon");
+const $open = $inputSearch.querySelector(".-open");
+const $close = $inputSearch.querySelector(".-close");
 
-$icon.addEventListener("click", event =>{
+$open.addEventListener("click", event =>{
 
     event.preventDefault();
 
         $inputSearch.classList.toggle("-active");
+        $open.classList.remove("-active");
+        $close.classList.add("-active");
+});
+
+$close.addEventListener("click", event =>{
+
+    event.preventDefault();
+
+        $inputSearch.classList.toggle("-active");
+        $open.classList.add("active");
+        $close.classList.remove("-active");
+
 });
