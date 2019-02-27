34d1670c4b8ceca4adf5afca6f1e5270b4cf6781
Laís Cavalcanti <cavalcantislais@gmail.com>
2019-02-27 16:27:15 -0300

    corrigindo bugs

--- js/input-search.js
+++ js/input-search.js
@@ -2,21 +2,16 @@ const $inputSearch = document.querySelector(".input-search");
 const $open = $inputSearch.querySelector(".-open");
 const $close = $inputSearch.querySelector(".-close");
 
-$open.addEventListener("click", event =>{
-
-    event.preventDefault();
-
-        $inputSearch.classList.toggle("-active");
-        $open.classList.remove("-active");
-        $close.classList.add("-active");
+$open.addEventListener("click", event => {
+  event.preventDefault();
+  $inputSearch.classList.toggle("-active");
+  $open.classList.remove("-active");
+  $close.classList.add("-active");
 });
 
-$close.addEventListener("click", event =>{
-
-    event.preventDefault();
-
-        $inputSearch.classList.toggle("-active");
-        $open.classList.add("active");
-        $close.classList.remove("-active");
-
+$close.addEventListener("click", event => {
+  event.preventDefault();
+  $inputSearch.classList.toggle("-active");
+  $close.classList.remove("-active");
+  $open.classList.add("-active");
 });
