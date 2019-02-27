107a4d8dee543395d505921d5f109da600f02800
Laís Cavalcanti <cavalcantislais@gmail.com>
2019-02-27 01:11:20 -0300

    corrigindo bugs

--- js/home.js
+++ js/home.js
@@ -1,11 +1,10 @@
-let $valorInicial = 10;
+let valorInicial = 10;
 const $second = document.querySelector(".-second");
 
 $second.addEventListener("click", handleClick);
 
     function handleClick (){
         const $carrinho = document.querySelector(".-last");
-            $valorInicial++;
 
             $carrinho.textContent = `Carrinho (${++valorInicial})`;
     }
